ed235accd0250677cf29fc1cea9fca0ae0798407
Austin Denny <austinbdenny@gmail.com>
2020-03-11 14:38:20 -0500

    Completed rest.

--- fullapp/routes/newdish.js
+++ fullapp/routes/newdish.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const router = express.Router();
 let db = require('../models/database.js');
+let bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.get('/newdish', (req, res) => {
-  res.render('newdish.ejs');
+  db.query('SELECT * FROM categories').then(results => {
+    res.render('newdish.ejs', {
+      categories: results
+    });
+  });
 });
 
 router.post('/newdish', (req, res) => {
-  res.send('post succesful');
+  let name = req.body.title;
+  let category = req.body.category;
+  let fooddescription = req.body.description;
+  let price = parseInt(req.body.price);
+  let imageurl = req.body.imageurl;
+  let course = '';
+  db.none(
+    'INSERT INTO restaurants (name, category, fooddescription, price, course, imageurl) VALUES ($1, $2, $3, $4, $5, $6)',
+    [name, category, fooddescription, price, course, imageurl]
+  )
+    .then(() => {
+      res.redirect('/dishes');
+    })
+    .catch(err => {
+      res.send(err);
+    });
 });
 module.exports = router;
